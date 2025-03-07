8b5dfc26717e8a81dd6d20f21d86bf62f4ade038
lebicahlz <lebicahlz2@gmail.com>
2025-03-07 22:22:06 +0800

    Webgl object renderer script
    script to render objects and webpage example

---
+++ webgl_app/head_content.js
@@ -0,0 +1,12 @@
+document.getElementById("head_content").innerHTML +=
+
+  '<meta charset="UTF-8">'+
+  '<meta name="description" content="lebicahlz website">'+
+  '<meta name="keywords" content="lebicahlz, code, programming, tech">'+
+  '<meta name="author" content="lebicahlz">'+
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+  '<!--<meta http-equiv="X-UA-Compatible" content="ie=edge">--!>'+
+  "<title>lebicahlz</title>"+
+  '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+
+  '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">'+
+  '<link rel="icon" href="assets/lebicahlz icon.png">'
