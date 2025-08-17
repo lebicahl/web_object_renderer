773dda083f133b81314bcc6541add1782392a6f6
Dex <lebicahlz2@gmail.com>
2025-08-17 08:56:47 +0800

    Update head_content.js

--- head_content.js
+++ head_content.js
@@ -1,12 +1,13 @@
 document.getElementById("head_content").innerHTML +=
 
   '<meta charset="UTF-8">'+
-  '<meta name="description" content="lebicahl website">'+
-  '<meta name="keywords" content="lebicahl, code, programming, tech">'+
-  '<meta name="author" content="lebicahl">'+
+  '<meta name="description" content="lebical website">'+
+  '<meta name="keywords" content="lebical, code, programming, tech">'+
+  '<meta name="author" content="lebical">'+
   '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
   '<!--<meta http-equiv="X-UA-Compatible" content="ie=edge">--!>'+
-  "<title>lebicahlz</title>"+
+  "<title>lebical</title>"+
   '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+
   '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">'+
-  '<link rel="icon" href="assets/lebicahlz icon.png">'
+  '<link rel="icon" href="assets/lebical icon.png">'
+
